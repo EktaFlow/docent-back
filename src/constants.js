315212152197f353621aaf262a2cd5b8564b700d
Mark Franciose <mark.franciose@gmail.com>
2018-12-02 19:07:10 -0600

    pull in collection name from ENV

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const CollectionName = "DocentTest";
-const ConnectionString = process.env.COSMOS_CONNECTION_STRING
+const CollectionName = process.env.COLLECTION_NAME;
+const ConnectionString = process.env.COSMOS_CONNECTION_STRING;
 
 module.exports = {
 	CollectionName,
