938fe9135da2bb22bc80372a8342edd3945a0e3c
Mark Franciose <mark.franciose@gmail.com>
2019-07-31 11:40:45 -0500

    envs updated

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const CollectionName = 'prod-assessments';
-const ConnectionString = 'mongodb://dmdii-cosmos:FLez8qvmgeCgIuvWWpFo9WWuwdCiSf13juQjmy1xhq54T9vxbJAmvTBB0EXLVckeWEK4Rm702F4DVqCmQOv1Aw%3D%3D@dmdii-cosmos.documents.azure.com:10255/?ssl=true';
+const CollectionName = process.env.COLLECTION_NAME;
+const ConnectionString = process.env.COSMOS_CONNECTION_STRING;
 
 module.exports = {
 	CollectionName,
